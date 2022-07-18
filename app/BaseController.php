f5a0b8266bb258287c12bdc7e566ffcaded109c5
phendan <10546560+phendan@users.noreply.github.com>
2022-07-18 21:40:44 +0200

    Move instantiation of User and Database Models to BaseController

--- app/BaseController.php
+++ app/BaseController.php
@@ -3,12 +3,24 @@
 namespace App;
 
 use App\View;
+use App\Models\Database;
+use App\Models\User;
 
 class BaseController {
+    protected Database $db;
+    protected User $user;
     protected View $view;
 
     public function __construct()
     {
+        $this->db = new Database;
+        $this->user = new User($this->db);
         $this->view = new View;
+    }
+
+    protected function redirectTo(string $path)
+    {
+        header('Location: ' . $path);
+        exit();
     }
 }
