18442b2b69d59b97f9bd86abb36077fa1b08421e
phendan <10546560+phendan@users.noreply.github.com>
2022-07-26 18:58:24 +0200

    Make BaseController abstract

--- app/BaseController.php
+++ app/BaseController.php
@@ -5,8 +5,9 @@ namespace App;
 use App\View;
 use App\Models\Database;
 use App\Models\User;
+use App\Request;
 
-class BaseController {
+abstract class BaseController {
     protected Database $db;
     protected User $user;
     protected View $view;
@@ -16,7 +17,13 @@ class BaseController {
         $this->db = new Database;
         $this->user = new User($this->db);
         $this->view = new View($this->user);
+
+        if ($this->user->isLoggedIn()) {
+            $this->user->find($this->user->getId());
+        }
     }
+
+    abstract public function index(Request $request);
 
     protected function redirectTo(string $path)
     {
